9bbd13683801b25e7282381f1b344bb216a368a9
Paulcou <paul.coutiere@hotmail.fr>
2019-04-20 23:57:50 +0200

    main

--- Piscine/main.cpp
+++ Piscine/main.cpp
@@ -5,35 +5,34 @@ int main()
 {
     SvgFile svgout;
 
-    Graphe g{"files/manhattan.txt", "files/manhattan_weights_0.txt"};
+    Graphe g{"files/triville.txt", "files/triville_weights_0.txt"};
 
     ///PARTIE 1
-    //svgout.addText(75, 45, "Chargement du graphe");
-    //svgout.addText(500, 45, "Couts totaux (prim) :");
 
-
-    ///Choisir quel prime
+    /// # Choisir quel prime
     //g.codePrim(0);
     //g.codePrimC2(0);
 
-    ///Affichage prime
+    /// # Affichage prime
     //g.dessinerGrapheChargement(&svgout);
     //g.afficherPrime(&svgout);
+    //svgout.addText(75, 45, "Chargement du graphe");
+    //svgout.addText(500, 45, "Couts totaux (prim) :");
 
     ///PARTIE 2
     g.dessinerGrapheChargementPareto(&svgout);
     g.codePareto(&svgout);
-    //g.dessinCalculGraphePareto(&svgout);
+    g.dessinCalculGraphePareto(&svgout);
 
     ///PARTIE 3
     /**g.compteurDjikstra();
     g.dessinerGrapheChargementDjikstra(&svgout);
-**/
+    **/
 
     ///PARTIE BONUS
 
-    ///  # Heuristique
-    g.dessinCalculHeuristique(&svgout);
+    /// # Heuristique
+    //g.dessinCalculHeuristique(&svgout);
 
     return 0;
 }
